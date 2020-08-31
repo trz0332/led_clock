170e346fa0f9915ff8f8c5bc0e9d8badc07b989c
trz0332 <trz0332@163.com>
2020-08-31 23:37:30 +0800

    使用农历
    会从nl_db文件中读取第一行的日期，然后根据当前日期推算出需要偏移指针来获取当前日期的农历

--- nl.py
+++ nl.py
@@ -1,13 +1,14 @@
 import time
-
 def nl(x):
     utime_m=time.mktime((x[0],x[1],x[2],0,0,0,0,0))
-    if utime_m<649468800:
-        return 000000
-    else:
-        seek_f=((utime_m-649468800)//86400)*15
-        with open('nl_db') as file :
-            sa=file.seek(seek_f)
+    with open('nl_db') as file :
+        st_m=file.readline(15)
+        utime_c=time.mktime((int("20"+st_m[:2]),int(st_m[2:4]),int(st_m[4:6]),0,0,0,0,0))
+        if utime_m<utime_c:
+            return 000000
+        else:
+            seek_f=((utime_m-utime_c)//86400)*15
+            file.seek(seek_f)
             rd_line=file.readline(20)
-    return rd_line[7:13]
+            return int(rd_line[7:13])
 
